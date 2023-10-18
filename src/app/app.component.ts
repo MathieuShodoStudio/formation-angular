5f7ff68476ef857fc45cbe1df76b8a5397270a43
Mathieu Shodo-Studio <135144997+MathieuShodoStudio@users.noreply.github.com>
2023-10-18 17:04:09 +0200

    Added call to HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'formation-angular';
+  meal$!: Observable<String>;
+
+  constructor(private readonly httpClient : HttpClient) {};
+
+  ngOnInit(): void {
+    this.meal$ = this.getMeal(this.idMeal);
+  };
+
+  idMeal = '52771';
+
+  getMeal(id : String) : Observable<String> {
+    const urlMeal = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+
+    return this.httpClient.get<Payload>(urlMeal)
+      .pipe(map((payload: Payload) => {
+        const {meals} = payload;
+        const myMeal = meals[0];
+        return myMeal.strMeal;
+      }));
+  }
+}
+
+interface Payload {
+  meals : Meal[];
+}
+
+interface Meal {
+  strMeal : String;
 }
